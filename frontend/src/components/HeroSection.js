fdbba5d7a9e35c6990630fcb9c1ec80fbc5ca1dc
builtbyjosh <pc.refuge@gmail.com>
2023-01-19 13:54:54 -0600

    cleaned up component

--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col, Stack, Container, Card } from 'react-bootstrap';
+import { Row, Col, Container, Button } from 'react-bootstrap';
 import bgImage from '../images/pexels-abby-chung-1134000.jpg';
 
 const HeroSection = () => {
@@ -13,26 +13,20 @@ const HeroSection = () => {
 
   return (
     <Container fluid>
-      <Row d-flex justify-content-center align-items-center vh-100>
-        <Col>
-          <Row className="align-items-center">
-            <Col
-              md="5"
-              className="d-none d-md-block "
-              style={sectionStyle}
-            ></Col>
-            <Col md="7">
-              <Container className='justify-items-center w-75 text-center' >
-                <h1 className=" text-dark text-align-middle">
-                  Welcome to LullaPay
-                </h1>
-                <p>
-                  Connecting Parents and Daycare Providers with easy payment
-                  solutions
-                </p>
-              </Container>
-            </Col>
-          </Row>
+      <Row className="d-flex justify-content-center align-items-center vh-100">
+        <Col md="5" className="d-none d-md-block " style={sectionStyle}></Col>
+        <Col md="7">
+          <Container className="justify-items-center w-75 text-center">
+            <h1 className=" text-dark text-align-middle">
+              Welcome to LullaPay
+            </h1>
+            <p>
+              Connecting Parents and Daycare Providers with easy payment
+              solutions
+            </p>
+          <Button>More Info</Button>
+          </Container>
+
         </Col>
       </Row>
     </Container>
